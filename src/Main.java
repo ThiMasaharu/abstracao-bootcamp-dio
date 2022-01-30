9852a94e1a40774f669f506e5aef8179814f000d
ThiMasaharu <thi.thiagosakugawa@gmail.com>
2022-01-30 18:59:15 -0300

    Terceiro commit

--- src/Main.java
+++ src/Main.java
@@ -11,9 +11,9 @@ public class Main {
         curso1.setCargaHoraria(8);
         
         Curso curso2 = new Curso();
-        curso1.setTitulo("Curso JS");
-        curso1.setDescricao("Descrição curso Java");
-        curso1.setCargaHoraria(4);
+        curso2.setTitulo("Curso JS");
+        curso2.setDescricao("Descrição curso Java");
+        curso2.setCargaHoraria(4);
         
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria de Java");
---
+++ src/dominio/desafio/Dev.java
@@ -0,0 +1,76 @@
+package dominio.desafio;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Dev {
+    private String nome;
+    private Set<Conteudo> conteudoInscritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+    
+    public void inscreverBootcamp(Bootcamp bootcamp){}
+    
+    public void progredir(){}
+    
+    public void calcularTotalXp(){}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudoInscritos() {
+        return conteudoInscritos;
+    }
+
+    public void setConteudoInscritos(Set<Conteudo> conteudoInscritos) {
+        this.conteudoInscritos = conteudoInscritos;
+    }
+
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 41 * hash + Objects.hashCode(this.nome);
+        hash = 41 * hash + Objects.hashCode(this.conteudoInscritos);
+        hash = 41 * hash + Objects.hashCode(this.conteudosConcluidos);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Dev other = (Dev) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.conteudoInscritos, other.conteudoInscritos)) {
+            return false;
+        }
+        if (!Objects.equals(this.conteudosConcluidos, other.conteudosConcluidos)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
